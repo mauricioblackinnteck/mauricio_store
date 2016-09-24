45b9ad2e25d42ad298b5e134934cbf0f265eb125
Mauricio <mauricio@blackinnteck.com>
2016-09-24 15:44:28 -0500

    todo

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,10 @@
 class Category < ActiveRecord::Base
+  uniq.map{|c| c.name}.each do |v| 
+    self.class.class_eval do
+      define_method v.downcase.split(" ").join("_") do 
+        find_by_name(v)
+      end
+    end
+  end
+  has_many :products
 end
--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,2 +1,9 @@
 class ProductType < ActiveRecord::Base
+	  uniq.map{|c| c.name}.each do |v| 
+    self.class.class_eval do
+      define_method v.downcase.split(" ").join("_") do 
+        find_by_name(v)
+      end
+    end
+  end
 end
